c98799d39e0161a8c068cfb6d63bb67b027b4968
Gildonghong <oilil7859@gmail.com>
2021-10-28 16:29:11 +0900

    해볼까

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var express = require('express')
-var app = express();
 
-app.get(function(req, res){
+var app = http.createServer(function(req, res){
     var _url = req.url
     var queryData = url.parse(_url, true).query;
     var corp = queryData.corp;
@@ -14,18 +12,9 @@ app.get(function(req, res){
     var filepath = "/home/ftp-user/ftp-file/" + corp + "/" + tname + "/" + wgroup + "/" + seq;
 
     console.log(filepath);
-
-    res.write("<!doctype html>");
-    res.write("<html>");
-    res.write("<head>");
-    res.write('<meta charset="UTF-8">');
-    res.write("    <title>급여명세서</title>");
-    res.write("</head>");
-    res.write("<body>");
-    res.write("<iframe src='./viewer.html?file=" + filepath + ".pdf'/>");
-    res.write("</body>");
-    res.write("</html>");
-    res.end();
+    
+    res.writeHead(200);
+    res.end(fs.readFileSync(__dirname + '/index.html?path=' + filepath));
 });
 
 app.listen(3000, () => console.log("Server Listener"));
