33961fb91a54925e6c839f242181e3dd6528e11e
Gildonghong <oilil7859@gmail.com>
2021-10-28 10:09:07 +0900

    test2222

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const { response } = require("express");
-const express = require("express");
-const fs = require('fs');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-const app = express();
-
-app.get("/", (req, res) => {
-    var corp = req.query.corp;
-    var tname = req.query.tname;
-    var wgroup = req.query.wgroup;
-    var seq = req.query.seq;
-
+const app = http.createServer(function(req, res){
+    var _url = req.url
+    var queryData = url.parse(_url, true).query;
+    var corp = queryData.corp;
+    var tname = queryData.tname;
+    var wgroup = queryData.wgroup;
+    var seq = queryData.seq;
     var filepath = "/home/ftp-user/ftp-file/" + corp + "/" + tname + "/" + wgroup + "/" + seq + ".pdf";
 
     console.log(filepath);
 
-    fs.readFile(filepath, 'utf-8', function(err, data){
+    fs.readFile(filepath, 'utf8', function(err, data){
         var template = "<!doctype html><html><title>급여명세서</title><meta charset='utf-8'></head><body>${data}</body></html>";
         res.send(template);
     });
