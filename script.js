6de6402b69ab6fb6a524398d032bc5a0407a5357
Gildonghong <oilil7859@gmail.com>
2021-11-01 16:00:55 +0900

    clean

--- script.js
+++
@@ -1,9 +0,0 @@
-// Code goes here
-
-let ifrm = document.getElementById('iframeid');
-console.log(ifrm);
-
-ifrm.onload = function () {
-  let doc = ifrm.contentWindow.document;
-  console.log(doc);
-}
