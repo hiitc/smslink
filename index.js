0f82a98e60c5ff6148d914fda86b61a32e07e5f5
Gildonghong <oilil7859@gmail.com>
2021-11-01 09:33:30 +0900

    upup

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express')
 var app = express()
 
+app.get('/', function(req, res){
+	var corp = req.query.corp;
+	var tname = req.query.tname;
+	var wgroup = req.query.wgroup;
+	var seq = req.query.seq;
+
+	var fpath = "/home/ftp-user/ftp-file/" + corp + "/" + tname + "/" + wgroup + "/" + seq
+
+	res.send("<iframe src='" + fpath + "'></iframe>");
+
+	console.log(fpath);
+})
+
 app.listen(3000, function(){
 	console.log("Server Listener Port 3000")
 });
