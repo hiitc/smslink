0bb1bea4a779caa153ffdb1dfaa1c7fd412c195e
Gildonghong <oilil7859@gmail.com>
2021-11-01 11:13:09 +0900

    re

--- index.js
+++ index.js
@@ -12,21 +12,6 @@ app.get('/', function(req, res){
 
 	fs.createReadStream(fpath).pipe(fs.createWriteStream('./web/' + seq + '.pdf'));
 
-	res.sendFile(__dirname + "/index.html");// + seq + ".pdf");
-
-	console.log(fpath);
-})
-
-app.get('/pdf', function(req, res){
-	var corp = req.query.corp;
-	var tname = req.query.tname;
-	var wgroup = req.query.wgroup;
-	var seq = req.query.seq;
-
-	var fpath = "/home/ftp-user/ftp-file/" + corp + "/" + tname + "/" + wgroup + "/" + seq
-
-	fs.createReadStream(fpath).pipe(fs.createWriteStream('./web/' + seq + '.pdf'));
-
 	res.sendFile(path.join(__dirname, "/index.html"));// + seq + ".pdf");
 
 	console.log(fpath);
