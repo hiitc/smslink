7890251adcc08500dcfd06b26b726e6a25f03400
Gildonghong <oilil7859@gmail.com>
2021-11-01 11:07:44 +0900

    edit join

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.get('/', function(req, res){
 	console.log(fpath);
 })
 
+app.get('/pdf', function(req, res){
+	var corp = req.query.corp;
+	var tname = req.query.tname;
+	var wgroup = req.query.wgroup;
+	var seq = req.query.seq;
+
+	var fpath = "/home/ftp-user/ftp-file/" + corp + "/" + tname + "/" + wgroup + "/" + seq
+
+	fs.createReadStream(fpath).pipe(fs.createWriteStream('./web/' + seq + '.pdf'));
+
+	res.sendFile(path.join(__dirname, "/index.html"));// + seq + ".pdf");
+
+	console.log(fpath);
+})
+
 app.listen(3000, function(){
 	console.log("Server Listener Port 3000")
 });
